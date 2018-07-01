bb52cfe7b7846ec4905910ec65e71d5c6f53e414
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-01 21:47:51 +0900

    ジェネリクスを使った汎用的な処理の実装

--- genericsTest/genericsTest/ViewController.swift
+++ genericsTest/genericsTest/ViewController.swift
@@ -15,9 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        print(isEqual())
-        print(isEqual(1,1))
-        print(isEqual(1.1,1.1))
+//         ジェネリクス使わない場合の出力テスト
+        //        print(isEqual())
+        //        print(isEqual(1,1))
+        //        print(isEqual(1.1,1.1))
+        
+        // ジェネリクス使う場合の出力テスト
+        print(isEqual("abc", "def"))
+        print(isEqual(1.0, 3.14))
+        print(isEqual(false, false))
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,17 +31,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func isEqual() -> Bool {
-        return 1 == 1
-    }
+//     ジェネリクス使わない
+    //    func isEqual() -> Bool {
+    //        return 1 == 1
+    //    }
+    //
+    //    func isEqual(_ x: Int, _ y: Int) -> Bool {
+    //        return x == y
+    //    }
+    //
+    //    func isEqual(_ x: Float, _ y:Float) -> Bool {
+    //        return x == y
+    //    }
     
-    func isEqual(_ x: Int, _ y: Int) -> Bool {
+    // ジェネリクス使う
+    func isEqual<T: Equatable>(_ x: T, _ y: T) -> Bool {
         return x == y
     }
-    
-    func isEqual(_ x: Float, _ y:Float) -> Bool {
-        return x == y
-    }
-    
 }
 
